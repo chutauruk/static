933bad66b3c542b297cf83f570ce73748351a9a4
Cynthia Hutauruk <cyn.uruk@gmail.com>
2022-10-19 15:00:25 -0600

    hmm

--- StaticMethodsProp/static.js
+++ StaticMethodsProp/static.js
@@ -1,1 +1,12 @@
-console.log('Static Methods and Properties')
+class Governor {
+    constructor(name, money, law, age){
+        this.name = name;
+        this.money = money;
+        this.law = law;
+        this.age = age;
+    }
+    static takeTax(personName){
+        console.log(this.name + " will take tax from " + personName);
+       }
+}
+Governor.takeTax("Will");
